90b7492c1f46aa48e43a5ae439d9488bc516616a
alokbg11 <alokbg11@gmail.com>
2019-04-03 12:57:07 -0400

    Initial Commit

---
+++ src/main/java/com/example/amazonCodeBuild/AmazonCodeBuildApplication.java
@@ -0,0 +1,13 @@
+package com.example.amazonCodeBuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonCodeBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonCodeBuildApplication.class, args);
+	}
+
+}
