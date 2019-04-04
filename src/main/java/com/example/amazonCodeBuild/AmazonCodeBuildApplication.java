309a7581bc2ac9a5b4799d3f8dbd0aba237c9a1f
alokbg11 <alokbg11@gmail.com>
2019-04-04 11:28:53 -0400

    Add get mapping test

--- src/main/java/com/example/amazonCodeBuild/AmazonCodeBuildApplication.java
+++ src/main/java/com/example/amazonCodeBuild/AmazonCodeBuildApplication.java
@@ -2,12 +2,25 @@ package com.example.amazonCodeBuild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class AmazonCodeBuildApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AmazonCodeBuildApplication.class, args);
+	}
+
+}
+
+@RestController
+class TestController{
+
+	@GetMapping("/hello")
+	public String test(){
+		return "holla";
 	}
 
 }
